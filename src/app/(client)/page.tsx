3955cae433bf42e3f27e46f9c0e14020c60955f8
Ilya Vasilev <vasilyevir@yandex.ru>
2025-03-09 13:29:18 +0300

    fix build

--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -3,7 +3,7 @@ import {GetBrand} from "@/api";
 
 export default async function Home() {
   const brands = await GetBrand();
-
+  console.log(brands);
   return (
     <div>
       <MainPageContent brands={brands} />

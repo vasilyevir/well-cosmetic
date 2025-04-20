bfb8799cef2c1f686427044bf152845a79180ed7
Ilya Vasilev <vasilyevir@yandex.ru>
2025-04-20 14:42:50 +0300

    feat: add new host

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,10 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
+        hostname: "new-well-cosmetics.vercel.app",
+      }
+      {
+        protocol: "https",
         hostname: "m7zas4xswxdvpdt7.public.blob.vercel-storage.com",
       },
     ],

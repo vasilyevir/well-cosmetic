1d6afbdd1d787a112655db6986a428a8cb1f6030
Ilya Vasilev <vasilyevir@yandex.ru>
2025-04-20 14:50:32 +0300

    fix: next config

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,10 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
+        hostname: "fqa5cmbcgti1knug.public.blob.vercel-storage.com",
+      },
+      {
+        protocol: "https",
         hostname: "m7zas4xswxdvpdt7.public.blob.vercel-storage.com",
       },
     ],

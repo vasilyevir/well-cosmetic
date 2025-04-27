8e72bed21d55de200a61d89558d35eef1ceabfc6
Ilya Vasilev <vasilyevir@yandex.ru>
2025-04-27 20:16:21 +0300

    fix: layout

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,7 +1,7 @@
 import { Header } from "@/components/Header";
 import { SidebarProvider } from "@/components/ui/sidebar";
-import SideBar from "@/widgets/SideBar";
 import { Footer } from "@/widgets/Footer";
+import SideBarPrivate from "@/widgets/SideBar/SideBarPrivate";
 
 export default function RootLayout({
   children,
@@ -11,7 +11,7 @@ export default function RootLayout({
   return (
     <>
       <SidebarProvider defaultOpen={false}>
-        <SideBar />
+        <SideBarPrivate />
         <main className="w-full overflow-x-auto h-full">
           <Header />
           <div className="m-auto mt-[70px] max-w-[1200px] p-2 md:p-4 lg:p-8 w-full overflow-auto min-h-[calc(100svh-70px)] h-full">
